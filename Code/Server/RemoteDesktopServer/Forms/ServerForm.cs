b798a3b8086c80b9d2edb36d86401b650eaeb025
dangkhoa0615 <khoand0615@ut.edu.vn>
2026-04-05 22:32:56 +0700

    Update ServerForm.cs

--- Code/Server/RemoteDesktopServer/Forms/ServerForm.cs
+++ Code/Server/RemoteDesktopServer/Forms/ServerForm.cs
@@ -9,7 +9,17 @@ namespace RemoteDesktopServer
 
         private void ServerForm_Load(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
