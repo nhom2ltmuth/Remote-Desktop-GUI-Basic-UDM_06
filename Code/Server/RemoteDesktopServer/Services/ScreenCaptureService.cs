1cb03b3ec0a6e90c01cb2e16c041b53c31ee03fc
Quang <ngphuquang010206@gmail.com>
2026-04-10 10:49:24 +0700

    add screen capture service

--- Code/Server/RemoteDesktopServer/Services/ScreenCaptureService.cs
+++ Code/Server/RemoteDesktopServer/Services/ScreenCaptureService.cs
@@ -1,10 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
 
 namespace RemoteDesktopServer.Services
 {
     internal class ScreenCaptureService
     {
+        public Bitmap CaptureScreen()
+        {
+            Rectangle bounds = Screen.PrimaryScreen.Bounds;
+            Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height);
+
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.CopyFromScreen(
+                    bounds.Location,
+                    Point.Empty,
+                    bounds.Size
+                );
+            }
+
+            return bitmap;
+        }
+
+        public byte[] CaptureScreenBytes()
+        {
+            using (Bitmap bitmap = CaptureScreen())
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
     }
 }
