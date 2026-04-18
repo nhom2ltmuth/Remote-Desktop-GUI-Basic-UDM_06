faf70740c0093fce412a99319137a0b2550f47da
Quang <ngphuquang010206@gmail.com>
2026-04-18 19:51:22 +0700

    update

--- Code/Client/RemoteDesktopClient/Forms/ClientForm.Designer.cs
+++ Code/Client/RemoteDesktopClient/Forms/ClientForm.Designer.cs
@@ -145,7 +145,7 @@
             picRemoteScreen.Size = new System.Drawing.Size(1200, 515);
             picRemoteScreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             picRemoteScreen.TabIndex = 4;
-            picRemoteScreen.TabStop = false;ừ 
+            picRemoteScreen.TabStop = false;
             // 
             // lblIpTitle
             // 
